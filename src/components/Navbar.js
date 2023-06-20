6f308668e272b326e5c22b9c8950545a04a3985b
Anand <samarth.anand@capgemini.com>
2023-06-20 16:22:47 +0530

    jwt auth working, signin/out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const logout = ()=>  {
+    localStorage.removeItem("token");
+    navigate("/login");
+  }
+
   return (
     <div className="bg-gray-800">
-      <div className="h-16 px-8 flex items-center">
+      <div className="h-16 px-8 flex ">
+        <div className="flex items-center">
         <a className="text-white font-bold" 
-        href="\">MediStock Management System</a>
+        href="\">MediStock Management System</a></div>
+        <div className="flex items-right">
+        <button type='submit' className="btn btn-primary btn-sm me-2" onClick={logout}>SignOut</button>
       </div>
+      </div>
+
+     
     </div>
   );
 };
--- src/components/home/Login.js
+++ src/components/home/Login.js
@@ -1,145 +1,206 @@
 import React from 'react'
+
 import {useEffect, useState} from "react"
+
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import AuthUser from '../service/AuthUser';
 //import { fetchUser } from '../Actions/UserAction';
-
 function Login() {
-   // const dispatch = useDispatch();
-   // const data = useSelector((state) => state.user)
 
+   // const dispatch = useDispatch();
+
+   // const data = useSelector((state) => state.user)
     const navigate = useNavigate();
-  //  const {http,setToken} = AuthUser();
-    const initialValues = {userName : "", password : ""}
+    const initialValues = {"userName" : "", "password" : ""}
     const [formValues, setFormValues] = useState(initialValues);
+    const [userName,setUserName] = useState("");
+    const [password,setPassword] = useState("");
     const [formErrors, setFormErrors] = useState({});
     const [isSubmit, setIsSubmit] = useState(false);
-  
-const API_URL = "http://localhost:8201/users/";
     const handleChange = (e)=>{
       const {name, value} = e.target
       setFormValues({...formValues, [name] : value});
     }
-  
     const handleSubmit = (e)=>{
       e.preventDefault();
-      setFormErrors(validate(formValues));
+      setUserName(formValues.userName);
+      setPassword(formValues.password);
+      setFormErrors(validate(formValues))
       setIsSubmit(true);
-      
     }
+
     useEffect(()=>{
-      console.log(formErrors)
+
+     
+
       if(Object.keys(formErrors).length === 0 && isSubmit){
-      //  dispatch(addTrainee(formValues));
-      axios
-      .post(API_URL + "token", { formValues })
-      .then((response) => {
-        if (response.data.accessToken) {
-       localStorage.setItem("token", JSON.stringify(response.data));
+
+        // dispatch(fetchUser(formValues.userName, formValues.password));
+
+        console.log("admin and home here");
+
+        axios
+
+        .post(`http://localhost:8201/users/token`,{userName,password})
+
+        .then((response) => {
+
+           // document.getElementById('loginAfter').innerHTML = 'Login Successful'
+
+           
+            console.log(response);
+           if(response.data)
+        {
+          localStorage.setItem("token", JSON.stringify(response.data));
+          navigate("/");
         }
+        // else{
+        //   alert("Username and Password not match to any existing user.");
+        // }
+    })
+        .catch((error) => {
+          console.log(error)
+         // document.getElementById('loginAfter').innerHTML = error.response.data.errorMessage
+        });
+
+      }
+
+      setIsSubmit(false);
+
+    },[formErrors])
+
+ 
+
+    const validate = (value)=>{
+
+      const errors = {}
+
+      if(!value.userName){
+
+        errors.userName = "Please provide userName"
+
+      }
+
+      if(!value.password){
+
+        errors.password = "Please provide password"
+
+      } else if (value.password.length < 4) {
+
+        errors.password = "Password must be more than 4 characters";
+
+      } else if (value.password.length > 10) {
+
+        errors.password = "Password cannot exceed more than 10 characters";
+
+      }
+
+      return errors;
+
+    }
+
+    return (
+
+      <div className="Login" data-testid="loginTest">
+
+        {/* {Object.keys(formErrors).length === 0 && isSubmit ? (
+
+          <div className="ui message success">Signed in successfully</div>
+
+        ) : (
+
+          <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+
+        )} */}
 
        
-      }).catch((error) =>{
-        console.log(error);
-      });
-      
-      setIsSubmit(false);
-     
-    
-    }
-    },[formErrors])
-  
-   
-  
-    // useEffect(()=>{
-      
-    //   if(Object.keys(formErrors).length === 0 && isSubmit){
-    //     // dispatch(fetchUser(formValues.username, formValues.password));
-    //     console.log("admin and home here");
-    //   // http
-    //   //   .post('/login',{userName:formValues.username,password:formValues.password})
-    //   //   .then((data) => {
-    //   //      // document.getElementById('loginAfter').innerHTML = 'Login Successful'
-    //   //       console.log(formValues)
-    //   //       // if(formValues.username === 'admin'){
-    //         //   navigate('/admin')
-    //         // }else{
-    //         //   navigate('/home');
-    //         // }
-        
-    // }
-    //     // .catch('(error) => {
-    //     //   // console.log(error)
-    //     //   document.getElementById('loginAfter').innerHTML = error.response.data.errorMessage
-    //     // });
-      
-    // },[formErrors])
-  
-    const validate = (value)=>{
-      const errors = {}
-      if(!value.userName){
-        errors.userName = "Please provide username"
-      }
-      if(!value.password){
-        errors.password = "Please provide password"
-      } else if (value.password.length < 4) {
-        errors.password = "Password must be more than 4 characters";
-      } else if (value.password.length > 10) {
-        errors.password = "Password cannot exceed more than 10 characters";
-      }
-      return errors;
-    }
-    return (
-      <div className="Login" data-testid="loginTest">
-        {/* {Object.keys(formErrors).length === 0 && isSubmit ? (
-          <div className="ui message success">Signed in successfully</div>
-        ) : (
-          <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
-        )} */}
-        
+
+
+
 
 
         <div className='container mx-auto my-8'>
+
         <form onSubmit={handleSubmit} className=" shadow-lg p-3 mb-5  rounded formView">
+
           <h3>Login Form</h3>
+
           <div className="ui divider"></div>
+
           <div className="ui form">
-            <div className="field">
-              <label>Username</label>
-              <input
-                type="text"
-                name="userName"
-                placeholder="Username"
-                value={formValues.username}
-                onChange={handleChange}
-              />
-            </div>
-            <p className='error'>{formErrors.userName}</p>
 
             <div className="field">
-              <label>Password</label>
+
+              <label>userName</label>
+
               <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                value={formValues.password}
+
+                type="text"
+
+                name="userName"
+
+                placeholder="userName"
+
+                value={formValues.userName}
+
                 onChange={handleChange}
+
               />
+
             </div>
+
+            <p className='error'>{formErrors.userName}</p>
+
+
+
+
+            <div className="field">
+
+              <label>Password</label>
+
+              <input
+
+                type="password"
+
+                name="password"
+
+                placeholder="Password"
+
+                value={formValues.password}
+
+                onChange={handleChange}
+
+              />
+
+            </div>
+
             <p className='error'>{formErrors.password}</p>
+
             <button type='submit' className="rounded bg-slate-600 text-white px-6 py-2 font-semibold">Sign In</button>
+
             <Link className="rounded bg-slate-600 text-white px-6 py-2 font-semibold" to='/sign-up'>SignUp</Link>
+
        
+
           </div>
+
           <div id='loginAfter'></div>
+
         </form>
+
         </div>
+
       </div>
-    
+
+   
+
     );
+
 }
+
+
+
 
 export default Login;
